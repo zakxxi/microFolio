acae123c1730e4b7894c016fe1d1ccdfdfd84b2f
zakxxi <adrien@startxxi.eu>
2012-11-05 14:59:55 +0100

    connected to flickr

--- js/main.js
+++ js/main.js
@@ -1,26 +1,63 @@
-$('#galery').bjqs({
-	    animtype      : 'slide',
-	            height        : 500,
-	            width         : 800,
-				animtype        : 'slide',
-				animduration    : 450,      // length of transition
-				animspeed       : 4000,     // delay between transitions
-				automatic       : true,     // enable/disable automatic slide rotation
+/*
+ * PARAMÈTRES  */
 
-				// control and marker configuration
-				showcontrols    : true,     // enable/disable next + previous UI elements
-				centercontrols  : true,     // vertically center controls
-				nexttext        : '',   // text/html inside next UI element
-				prevtext        : '',   // text/html inside previous UI element
-				showmarkers     : false,     // enable/disable individual slide UI markers
-				centermarkers   : false,     // horizontally center markers
+var apiKey = 'd6ced05a5e11bf668ecd946bba23b4c9';
+var albumId = '72157631550752681';
+var userId = '86735532@N05';
+var nbPhotos = 10;
+var photoSize = "_c"
 
-				// interaction values
-				keyboardnav     : false,     // enable/disable keyboard navigation
-				hoverpause      : true,     // enable/disable pause slides on hover
+//'72157631440828014';
 
-				// presentational options
-				usecaptions     : true,     // enable/disable captions using img title attribute
-				randomstart     : false,     // start from a random slide
-				responsive      : false     // enable responsive behaviour
+/*
+ * REQUETE AJAX VERS PHOTOS FLICKR
+ */
+
+$.getJSON('http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id=' + albumId + '&extras=url' + photoSize + '%2C&per_page=' + nbPhotos + '&format=json&nojsoncallback=1', function(data) {
+	$.each(data.photoset.photo, function(i, item) {
+
+		var photoUrl = item.url_c;
+		var photoTitle = item.title;
+
+		console.log(data)
+
+		var insertGal = '<li><img src="' + photoUrl + '" title="' + photoTitle + '" ></li>';
+
+		$(insertGal).appendTo('.bjqs');
+
+	});
+
+	// Slideshow 1
+	
+	$('#galery').bjqs({
+		    animtype      : 'slide',
+		            height        : 500,
+		            width         : 800,
+					animtype        : 'slide',
+					animduration    : 450,      // length of transition
+					animspeed       : 4000,     // delay between transitions
+					automatic       : true,     // enable/disable automatic slide rotation
+
+					// control and marker configuration
+					showcontrols    : true,     // enable/disable next + previous UI elements
+					centercontrols  : true,     // vertically center controls
+					nexttext        : '',   // text/html inside next UI element
+					prevtext        : '',   // text/html inside previous UI element
+					showmarkers     : false,     // enable/disable individual slide UI markers
+					centermarkers   : false,     // horizontally center markers
+
+					// interaction values
+					keyboardnav     : false,     // enable/disable keyboard navigation
+					hoverpause      : true,     // enable/disable pause slides on hover
+
+					// presentational options
+					usecaptions     : true,     // enable/disable captions using img title attribute
+					randomstart     : false,     // start from a random slide
+					responsive      : false     // enable responsive behaviour
+	});
+	
+
 });
+
+
+
