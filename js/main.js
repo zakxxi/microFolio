fc87757ae2d2d5ff083416cfaf277ff3f5af0258
zakxxi <adrien@startxxi.eu>
2012-11-05 15:56:41 +0100

    basic nav

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var albumId = '72157631550752681';
 var userId = '86735532@N05';
 var nbPhotos = 10;
 var photoSize = "_c"
+var delay = 500;
 
 //'72157631440828014';
 
@@ -32,7 +33,7 @@ $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photosets.getPhoto
 		            height        : 500,
 		            width         : 800,
 					animtype        : 'slide',
-					animduration    : 450,      // length of transition
+					animduration    : delay,      // length of transition
 					animspeed       : 4000,     // delay between transitions
 					automatic       : true,     // enable/disable automatic slide rotation
 
@@ -65,23 +66,92 @@ hover('#ne');
 hover('#se');
 hover('#sw');
 
+$('#nw').click(function(){
+	
+	$(this).addClass('active');
+	$('#nw>h1').addClass('active');
+	
+	$('#ne').removeClass('active');
+	$('#ne>h1').removeClass('active');
+	$('#se').removeClass('active');
+	$('#se>h1').removeClass('active');
+	$('#sw').removeClass('active');
+	$('#sw>h1').removeClass('active');
+	
+	$('#galery').fadeIn(delay);
+	
+
+})
+
+
+$('#ne').click(function(){
+	
+	$(this).addClass('active');
+	$('#ne>h1').addClass('active');
+	
+	$('#nw').removeClass('active');
+	$('#nw>h1').removeClass('active');
+	$('#se').removeClass('active');
+	$('#se>h1').removeClass('active');
+	$('#sw').removeClass('active');
+	$('#sw>h1').removeClass('active');
+	
+	$('#galery').fadeOut(delay);
+
+})
+
+$('#sw').click(function(){
+	
+	$(this).addClass('active');
+	$('#sw>h1').addClass('active');
+	
+	$('#ne').removeClass('active');
+	$('#ne>h1').removeClass('active');
+	$('#se').removeClass('active');
+	$('#se>h1').removeClass('active');
+	$('#nw').removeClass('active');
+	$('#nw>h1').removeClass('active');
+	
+	$('#galery').fadeOut(delay);
+	
+	
+
+})
+
+$('#se').click(function(){
+	
+	$(this).addClass('active');
+	$('#se>h1').addClass('active');
+	
+	$('#ne').removeClass('active');
+	$('#ne>h1').removeClass('active');
+	$('#nw').removeClass('active');
+	$('#nw>h1').removeClass('active');
+	$('#sw').removeClass('active');
+	$('#sw>h1').removeClass('active');
+	
+	$('#galery').fadeOut(delay);
+	
+	
+
+})
+
+
 
 function hover(id){
 	
-	if ($(id).hasClass('active')) {
-		
-	}
-	
-	else {
-		
+
 		$(id).mouseover(function() {
-			$(id+'>h1').addClass('active');
+			$(this).addClass('over');
+			$(id+'>h1').addClass('over');
 		});
 
 		$(id).mouseout(function() {
-			$(id+'>h1').removeClass('active');
+			$(this).removeClass('over');
+			$(id+'>h1').removeClass('over');
+			
 		});
-	}
+
 	
 
 }
